d31a2e6bf130bd7453fe80839b19de4496f09279
ivan-saji <ivansajimeleth@gmail.com>
2026-05-13 13:11:43 +0530

    used condtional expression in name.

--- Terraform/10-Dynamic Expressions/create_nsg.tf
+++ Terraform/10-Dynamic Expressions/create_nsg.tf
@@ -1,8 +1,10 @@
 resource "azurerm_network_security_group" "testnsg01" {
-  name                = "testnsg01"
+# using ternary operator to set the name of NSG based on location variable
+  name                = var.location == "Central India" ? "IndiaNSG" : "OutSideNSG"
   location            = var.location
   resource_group_name = var.resource_group_name
 
+#using dynamic block to create security rules based on the local variable nsg_rules which is a map of security rules
   dynamic "security_rule" {
 
     for_each = local.nsg_rules
