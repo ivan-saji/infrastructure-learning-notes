a6a6a988f5828793fd543c83c80b0d008039b70d
ivan-saji <ivansajimeleth@gmail.com>
2026-06-01 12:24:39 +0530

    Using data sources

---
+++ Terraform/12-DataSources/main.tf
@@ -0,0 +1,72 @@
+variable "prefix" {
+  default = "day13"
+}
+
+resource "azurerm_resource_group" "rg-shared" {
+  name     = "rg-shared-resources"
+  #this will take the location of resource group created in azure and use it for the resource group created by terraform
+  location = data.azurerm_resource_group.rg-shared.location
+}
+
+#using datasources to reference the existing Virtual Network and Subnet present in azure
+
+data "azurerm_virtual_network" "tf-vnet-shared" {
+  name                = "tf-vnet-shared"
+  resource_group_name = rg-shared.rg-shared.name
+}
+
+data "azurerm_subnet" "tf-vm-subnet" {
+  name                 = "tf-subnet-shared"
+  virtual_network_name = data.azurerm_virtual_network.tf-vnet-shared.name
+  resource_group_name = data.azurerm_virtual_network.tf-vnet-shared.resource_group_name
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = rg-shared.rg-shared.location
+  resource_group_name = rg-shared.rg-shared.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = data.azurerm_subnet.tf-vm-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = rg-shared.rg-shared.location
+  resource_group_name   = rg-shared.rg-shared.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
